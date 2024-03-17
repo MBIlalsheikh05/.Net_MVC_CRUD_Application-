9760a7d3fc152156f79f14845d94ab2c9d922fcc
MBIlalsheikh05 <140721272+MBIlalsheikh05@users.noreply.github.com>
2024-03-17 16:42:48 +0500

    Add files via upload

---
+++ Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Crud_MVC_Web_Application.Models;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? Category { get; set; }
+
+    public decimal? Price { get; set; }
+}
